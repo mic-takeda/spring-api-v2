f7fc26bf163b98bdedd3e516bd77629431512376
mic-takeda <131126959+mic-takeda@users.noreply.github.com>
2024-09-03 13:33:14 +0900

    DB取得＆接続設定の追加

---
+++ src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class UserServiceImpl implements UserService{
+    private final UserRepository userRepository;
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+}
